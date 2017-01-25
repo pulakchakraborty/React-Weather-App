983b13f9c8c63f41d63cc0e3f63eed3cfe95fa99
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2017-01-25 10:57:46 +0100

    Added activeClassNAme and activeStyle to highlight and show the active tag in bold

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,9 +6,9 @@ var Nav = React.createClass({
     return (
       <div>
         <h2>Navigation component</h2>
-        <Link to="/">Get Weather</Link>
-        <Link to="about">About</Link>
-        <Link to="examples">Examples</Link>
+        <Link to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</Link>
+        <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+        <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
       </div>
     );
   }
--- public/bundle.js
+++ public/bundle.js
@@ -26592,17 +26592,17 @@
 	      ),
 	      React.createElement(
 	        Link,
-	        { to: '/' },
+	        { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'Get Weather'
 	      ),
 	      React.createElement(
 	        Link,
-	        { to: 'about' },
+	        { to: 'about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'About'
 	      ),
 	      React.createElement(
 	        Link,
-	        { to: 'examples' },
+	        { to: 'examples', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'Examples'
 	      )
 	    );
